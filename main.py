8a7c2b739c065f8734f56234d8af45623e1da832
Oussama Rafouk <156216265+ocryptocode@users.noreply.github.com>
2025-02-02 16:40:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import torch
 import requests
-
+import sqlalchemy
 
 
 # design the custom pipeline for class students
@@ -8,3 +8,4 @@ class AIPipeline (self, name , age):
     self.name = name
     self.age = age
 
+def signup
